44c4e85b1e6e443f88474476b4686735348884e3
Marius Mandal <mariusmandal@gmail.com>
2020-08-26 09:58:17 +0200

    Update some.controller.php

--- controller/some.controller.php
+++ controller/some.controller.php
@@ -4,9 +4,7 @@ use UKMNorge\Slack\API\App;
 use UKMNorge\Slack\Cache\Team\Teams;
 use UKMNorge\Some\Forslag\Ideer;
 
-$teams = new Teams();
-
-Teams::getBySlackId( SLACK_UKMMEDIA_TEAM_ID );
+$team = Teams::getBySlackId( SLACK_UKMMEDIA_TEAM_ID );
 App::initFromBotToken( $team->getBot()->getAccessToken() );
 
 if( isset($_GET['forslag'])) {
